20dbd835cd7444e8417d419cac813501f40d65cb
edd-onyango <eddonyango.dev@gmail.com>
2024-01-05 19:13:06 +0300

    Add loop to the game function that allows us to play the game for 5 rounds.

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
 function launchGame () {
-   let playerChoice = prompt("Please input your choice and press enter").toLowerCase();
-   let computerChoice = getComputerChoice();
-   playGame(playerChoice, computerChoice);
-   
+   let noOfPlays = 1;
+   let computerWinCounter = 0;
+   let playerWinCounter = 0;
+   let drawCounter = 0;
+
+   while (noOfPlays <= 5){
+      let playerChoice = prompt("Please input your choice and press enter").toLowerCase();
+      let computerChoice = getComputerChoice();
+      let winner = playGame(playerChoice, computerChoice);
+
+      console.log(`Round ${noOfPlays}`);
+
+      if (winner == 1) {
+         playerWinCounter++;
+      } else if (winner == 0){
+         computerWinCounter++;
+      } else {
+         drawCounter++;
+      }
+
+      noOfPlays++;
+   }
+
+   if (playerWinCounter > computerWinCounter){
+      console.log(`You won the game:`);
+      console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
+   } else if (computerWinCounter > playerWinCounter){
+      console.log(`The computer won the game:`);
+      console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
+   } else {
+      console.log(`You drew the game:`);
+      console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
+   }
 }
 
 function getComputerChoice () {
@@ -14,41 +43,36 @@ function getComputerChoice () {
 
 
 function playGame (playerSelection, computerSelection) {
-   choiceRange = ["rock", "paper", "scissors"]
-   if ((choiceRange.includes(playerSelection))) {
-      // playerSelection = prompt("Kindly choose either 'Rock', 'Paper', or 'Scissors'");
-      if(playerSelection == computerSelection) {
-         console.log(`The player choose ${playerSelection} while the computer choose ${computerSelection}`);
-         console.log("You drew the game, let's play another round");
-         launchGame();
-      } 
-
-      else if ((playerSelection == "rock" ) && (computerSelection == "scissors")) {
-         console.log(`The player choose ${playerSelection} while the computer choose ${computerSelection}`);
-         console.log("You won the game! Hurray!");
+   const choiceRange = ["rock", "paper", "scissors"];
+      if ((choiceRange.includes(playerSelection))) {
+         if(playerSelection == computerSelection) {
+            console.log(`You choose ${playerSelection} while the computer choose ${computerSelection}.Draw.`);
+            return -1;
+         } 
+   
+         else if ((playerSelection == "rock" ) && (computerSelection == "scissors")) {
+            console.log(`You choose ${playerSelection} while the computer choose ${computerSelection}.Win.`);
+            return 1;
+         }
+   
+         else if ((playerSelection == "scissors" ) && (computerSelection == "paper")) {
+            console.log(`You choose ${playerSelection} while the computer choose ${computerSelection}.Win.`);
+            return 1;
+         }
+   
+         else if ((playerSelection == "paper" ) && (computerSelection == "rock")) {
+            console.log(`You choose ${playerSelection} while the computer choose ${computerSelection}.Win.`);
+            return 1;
+            
+         }
+         else {
+            console.log(`You choose ${playerSelection} while the computer choose ${computerSelection}.Lose.`);
+            return 0;
+         }
+      } else {
+         console.log("Only input either 'Rock', 'Paper' or 'Scissors'. Restarting the game")
          launchGame();
       }
-
-      else if ((playerSelection == "scissors" ) && (computerSelection == "paper")) {
-         console.log(`The player choose ${playerSelection} while the computer choose ${computerSelection}`);
-         console.log("You won the game! Hurray.");
-         launchGame();
-      }
-
-      else if ((playerSelection == "paper" ) && (computerSelection == "rock")) {
-         console.log(`The player choose ${playerSelection} while the computer choose ${computerSelection}`);
-         console.log("You won the game! Hurray.");
-         launchGame();
-      }
-      else {
-         console.log(`The player choose ${playerSelection} while the computer choose ${computerSelection}`);
-         console.log("You lost the game!");
-         launchGame();
-      }
-   } else {
-      console.log("Only input either 'Rock', 'Paper' or 'Scissors'");
-      launchGame();
-   }
 }
 
 launchGame();
