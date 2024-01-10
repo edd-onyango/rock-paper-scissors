be0e1e6c127e7f270ee27076e9c654876ddb9d8b
edd-onyango <eddonyango.dev@gmail.com>
2024-01-10 16:34:29 +0300

    Add button click event listeners to the game

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
 function launchGame () {
-   let noOfPlays = 1;
+   
    let computerWinCounter = 0;
    let playerWinCounter = 0;
    let drawCounter = 0;
 
-   while (noOfPlays <= 5){
-      let playerChoice = prompt("Please input your choice and press enter").toLowerCase();
-      let computerChoice = getComputerChoice();
-      let winner = playGame(playerChoice, computerChoice);
+   document.querySelectorAll('button').forEach(occurence => {
+         let id = occurence.getAttribute('id');
+         occurence.addEventListener('click', function() {
+            let playerChoice = this.id;
+            let computerChoice = getComputerChoice();
+            let winner = playGame(playerChoice, computerChoice);
+         });
+   }); 
+   
+   // console.log(`Round ${noOfPlays}`);
 
-      console.log(`Round ${noOfPlays}`);
+   // if (winner == 1) {
+   //    playerWinCounter++;
+   // } else if (winner == 0){
+   //    computerWinCounter++;
+   // } else {
+   //    drawCounter++;
+   // }
 
-      if (winner == 1) {
-         playerWinCounter++;
-      } else if (winner == 0){
-         computerWinCounter++;
-      } else {
-         drawCounter++;
-      }
-
-      noOfPlays++;
-   }
-
-   if (playerWinCounter > computerWinCounter){
-      console.log(`You won the game:`);
-      console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
-   } else if (computerWinCounter > playerWinCounter){
-      console.log(`The computer won the game:`);
-      console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
-   } else {
-      console.log(`You drew the game:`);
-      console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
-   }
+   // if (playerWinCounter > computerWinCounter){
+   //    console.log(`You won the game:`);
+   //    console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
+   // } else if (computerWinCounter > playerWinCounter){
+   //    console.log(`The computer won the game:`);
+   //    console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
+   // } else {
+   //    console.log(`You drew the game:`);
+   //    console.log(`Computer: ${computerWinCounter} Player: ${playerWinCounter} Draw:${drawCounter}`);
+   // }
 }
 
 function getComputerChoice () {
@@ -43,8 +44,6 @@ function getComputerChoice () {
 
 
 function playGame (playerSelection, computerSelection) {
-   const choiceRange = ["rock", "paper", "scissors"];
-      if ((choiceRange.includes(playerSelection))) {
          if(playerSelection == computerSelection) {
             console.log(`You choose ${playerSelection} while the computer choose ${computerSelection}.Draw.`);
             return -1;
@@ -69,10 +68,6 @@ function playGame (playerSelection, computerSelection) {
             console.log(`You choose ${playerSelection} while the computer choose ${computerSelection}.Lose.`);
             return 0;
          }
-      } else {
-         console.log("Only input either 'Rock', 'Paper' or 'Scissors'. Restarting the game")
-         launchGame();
-      }
 }
 
 launchGame();
